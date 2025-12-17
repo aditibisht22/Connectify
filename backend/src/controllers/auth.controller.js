8834ccfb0ad362a4ecbf719518a6a2709308ef96
aditibisht22 <aditi.bisht.2227@gmail.com>
2025-12-18 02:01:30 +0530

    signup endpoint

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,11 +1,66 @@
-export async function signup(req,res){
-    res.send("Signup Route");
+import User from "../models/Users.js";
+import jwt from "jsonwebtoken";
+
+export async function signup(req, res) {
+  const { email, password, fullName } = req.body;
+
+  try {
+    if (!email || !password || fullName) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long" });
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+
+    //email already exists
+    const existingUser = await User.findOne({email});
+    if(existingUser){
+        return res.status(400).json({message: "Email already exists, please use a different one"});
+    }
+
+    const idx = Math.floor(Math.random() * 100) + 1; //generates a num between 1 and 100
+    const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
+
+    const newUser = await User.create({
+        email,
+        fullName,
+        password,
+        profilePic: randomAvatar,
+    })
+
+    //unique identifier: to identify who owns this token
+    const token = jwt.sign({userId:newUser._id}, process.env.JWT_SECRET_KEY, {
+        expiresIn: "7d"
+    }
+    );
+
+    res.cookie("jwt", token, {
+        maxAge: 7*24*60*60*1000, //7d in ms
+        httpOnly: true, //prevent XSS attacks,
+        sameSite: "strict", //prevent CSRF attacks
+        secure: process.env.NODE_ENV === "production" // prevent HTTP requests
+    })
+
+    res.status(201).json({success: true, user: newUser})
+  } catch (error) {
+    console.log("Error in signup controller", error);
+    res.status(500).json({message:"Internal Server Error"});
+  }
 }
 
-export async function login(req,res){
-    res.send("Login Route");
+export async function login(req, res) {
+  res.send("Login Route");
 }
 
-export function logout(req,res){
-    res.send("Logout Route");
+export function logout(req, res) {
+  res.send("Logout Route");
 }
