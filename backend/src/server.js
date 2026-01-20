7d0df8de994d28c3a1a3bd021d3e4630e20eabfa
Aditi Bisht <aditi.bisht.2227@gmail.com>
2026-01-20 19:46:42 +0530

    user route

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 
 
 import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js"
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/auth", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
