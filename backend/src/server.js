2f686b0d95f46890ba7575733827f5222d328021
Aditi Bisht <aditi.bisht.2227@gmail.com>
2026-01-20 20:13:18 +0530

    chat routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,8 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js"
+import chatRoutes from "./routes/chat.route.js"
+
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -16,7 +18,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/auth", userRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
